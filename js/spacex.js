6ab41ff9d72774473292ba8ba3d18c1ca37bd7dd
GlebSuvakin <iitikbo0216suvakin.g@gmail.com>
2020-08-24 12:43:49 +0300

    Init

---
+++ js/spacex.js
@@ -0,0 +1,55 @@
+window.onload = () => {
+  fetch('https://api.spacexdata.com/v3/missions')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    let txt = JSON.stringify(data);
+    let obj = JSON.parse(txt);
+    console.log(obj);
+
+    let name = [];
+    let id = [];
+    let twit_url = []; 
+    let website_url = [];
+    let wiki_url = []; 
+    let descr = [];
+    let web = [];
+    let twit = [];
+    let wiki = [];
+    
+    for (let i = 0; i < obj.length; i++){
+      
+      id[i] = obj[i].mission_id;
+      name[i] = obj[i].mission_name;
+      twit_url[i] = obj[i].twitter;
+      website_url[i] = obj[i].website;
+      wiki_url[i] = obj[i].wikipedia;
+      descr[i] = obj[i].description;
+      document.getElementById('missionName'+i).innerHTML = name[i];
+      document.getElementById('missionId'+i).innerHTML = id[i];
+      document.getElementById('missionDescr'+i).innerHTML = descr[i];
+      web[i] = document.getElementById("missionWeb"+i);
+      twit[i] = document.getElementById("missionTwitter"+i);
+      wiki[i] = document.getElementById("missionWiki"+i);
+      if (website_url[i]){
+        web[i].setAttribute('href', website_url[i]);
+      }
+      if (twit_url[i]){
+        twit[i].setAttribute('href', twit_url[i]);
+      }
+      if (wiki_url[i]){
+        wiki[i].setAttribute('href', wiki_url[i]);
+      }
+    }
+    
+    /*console.log(name);
+    console.log(id);
+    console.log(twit_url);
+    console.log(website_url);
+    console.log(wiki_url);*/
+  });
+}
+
+//https://api.spacexdata.com/v3/missions
